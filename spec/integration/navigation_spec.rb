5ebc38e43b182c16c10fe8fac00522489e73dc69
Yomi Colledge <baphled@boodah.net>
2011-04-28 14:36:05 +0100

    Added spec for making sure links can be displayed as html when sending messages

--- spec/integration/navigation_spec.rb
+++ spec/integration/navigation_spec.rb
@@ -1,8 +1,5 @@
 require 'spec_helper'
 
-def sign_in
-  
-end
 describe "Navigation" do
   include Capybara
   
@@ -28,6 +25,23 @@ describe "Navigation" do
     page.should have_content "foobar: my comment"
   end
   
+  it "should convert links into a tags" do
+    @user = User.create!(:username => 'foobar', :email => 'whoami@me.com', :password => 'foobar')
+    
+    visit '/users/sign_in'
+    
+    fill_in 'Username', :with => @user.username
+    fill_in 'Password', :with => @user.password
+    
+    click_button 'Sign in'
+    
+    visit '/chat-engine'
+    
+    fill_in 'chat_engine_message_body', :with => '[a link](http://google.com "a link")'
+    click_button 'Send'
+    save_and_open_page
+    page.should have_content "foobar: a link"
+  end
   it "displays a list of signed in users"
   it "can select a message color"
   it "can send out invitations to join"
